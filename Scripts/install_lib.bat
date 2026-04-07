52e97b7fe99ef7497d1d8ee9cad68a53d7307690
RealGalaxyCat <111510472+RealGalaxyCat@users.noreply.github.com>
2026-04-07 23:26:00 +0200

    update comment

--- Scripts/install_lib.bat
+++ Scripts/install_lib.bat
@@ -1,2 +1,2 @@
-:: Used to install the gisa_api package in the current venv during development, so that autocomplete can be used.
+:: Used to install the gisa_api package in the current venv during development, so that autocomplete can be used in the IDE.
 pip install -e .
